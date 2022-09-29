9491a50e5a4ac3a0b5c22af68525d0e5f79161e1
JoyChudasama <joychudasama11@example.com>
2022-09-29 16:28:35 -0300

    added dependencies; init app setup

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+
+const app = express();
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+        console.log('MongoDB connected...')
+    }
+)
+
+app.use(express.json())
+app.use(helmet());
+app.use(morgan('common'));
+
+app.get('/', (req, res) => {
+    res.send('Use /api routes')
+})
+
+app.listen(8800, () => {
+    console.log("Server is running...")
+})
